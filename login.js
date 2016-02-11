3d3a505715f4dd2399642091e4b12d58893f0736
Arpith Siromoney <arpith@feedreader.co>
2016-02-11 15:19:21 +0530

    fix js style

--- login.js
+++ login.js
@@ -1,35 +1,42 @@
-var token = ''
-, user = ''
-, form = document.getElementById('loginForm')
-, login = document.getElementById('login')
-, base64_encode = function(str) {
-  return window.btoa(unescape(encodeURIComponent(str)))
+var token = '';
+var user = '';
+var form = document.getElementById('loginForm');
+var login = document.getElementById('login');
+
+function base64_encode(str) {
+  return window.btoa(unescape(encodeURIComponent(str)));
 }
-, api = function(method,endpoint,onload,params) {
-  var xhr = new XMLHttpRequest()
-  xhr.open(method,'https://api.feedreader.co/v1/'+endpoint)
-  xhr.setRequestHeader('authorization','Basic '+base64_encode(token+':'))
-  xhr.onload = onload
+
+function api(method, endpoint, onload, params) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, 'https://api.feedreader.co/v1/' + endpoint);
+  xhr.setRequestHeader('authorization', 'Basic ' + base64_encode(token + ':'));
+  xhr.onload = onload;
   if (params) {
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    xhr.send(params)
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+  } else {
+    xhr.send();
   }
-  else xhr.send()
 }
-, get_token = function() {
-  user = form.elements["user"].value
-  api('POST',user+'/tokens',function(){
+
+function get_token() {
+  user = form.elements["user"].value;
+  api('POST', user + '/tokens', function() {
     if ((hash = JSON.parse(this.responseText).token)) {
-      form.style.display = 'none'
-      localStorage.token = hash
-      localStorage.user = user
-      window.location.pathname = user
+      form.style.display = 'none';
+      localStorage.token = hash;
+      localStorage.user = user;
+      window.location.pathname = user;
+    } else {
+      alert('Please type them out again');
     }
-    else alert('Please type them out again')
-  },"user="+user+"&password="+form.elements["password"].value)
-  return false
+  }, "user=" + user + "&password=" + form.elements["password"].value);
+  return false;
 }
-login.onclick = get_token
+
+login.onclick = get_token;
+
 window.onbeforeunload = function() {
-  api('DELETE')
-}
+  api('DELETE');
+};

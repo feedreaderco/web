acf5027fb9617cb2c4aa1c7e7c0a5081ba3194be
Arpith Siromoney <arpith@feedreader.co>
2016-08-14 14:52:38 -0400

    on successful signup go to welcome page

--- signup.js
+++ signup.js
@@ -6,12 +6,15 @@ var signupButton = document.getElementById('signup');
 
 function signup() {
   user = form.elements["user"].value;
+  localStorage.user = user;
   api('POST', 'signup', function(response) {
-    if (response.url) {
-      window.location.href = response.url;
+    if (response.token) {
+      form.style.display = 'none';
+      localStorage.token = response.token;
+      window.location.pathname = 'https://feedreader.co/welcome/';
     } else {
       console.log(response);
-      alert('Something went wrong - please contact us');
+      alert('Something went wrong - please contact me at arpith@feedreader.co');
     }
   }, "user=" + user + "&password=" + form.elements["password"].value);
   return false;

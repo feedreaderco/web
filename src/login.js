c7ea92cc7b5520057c863d0c826ac79f9fa22b5a
Arpith Siromoney <arpith@feedreader.co>
2017-04-14 22:27:59 +0530

    login.js: es6 + new api lib

--- src/login.js
+++ src/login.js
@@ -1,26 +1,20 @@
 import api from './api';
 
-var user = '';
-var form = document.getElementById('loginForm');
-var login = document.getElementById('login');
-
-function get_token() {
-  user = form.elements["user"].value;
-  api('POST', user + '/tokens', function(response) {
-    if (response.token) {
-      form.style.display = 'none';
-      localStorage.token = response.token;
-      localStorage.user = user;
-      window.location.pathname = user + '/feeds';
-    } else {
-      alert('Please type them out again');
-    }
-  }, "user=" + user + "&password=" + form.elements["password"].value);
+function getToken() {
+  const form = document.getElementById('loginForm');
+  const login = document.getElementById('login');
+  const user = form.elements.user.value;
+  const password = form.elements.password.value;
+  api(user).user.tokens.create(password).then((response) => {
+    if (!response.token) throw '';
+    form.style.display = 'none';
+    localStorage.token = response.token;
+    localStorage.user = user;
+    window.location.pathname = `${user}/feeds`;
+  }).catch(() => {
+    alert('Please type that out again');
+  });
   return false;
 }
 
-login.onclick = get_token;
-
-window.onbeforeunload = function() {
-  api('DELETE');
-};
+login.onclick = getToken;

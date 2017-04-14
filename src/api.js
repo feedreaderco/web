825ba66efea879b779a5e360bd889e3ff3125a9a
Arpith Siromoney <arpith@feedreader.co>
2017-04-14 20:39:35 +0530

    support labels/folders

--- src/api.js
+++ src/api.js
@@ -1,16 +1,52 @@
 import fetch from 'isomorphic-fetch';
 
-function api(endpoint, fetchParams) {
+function singleParamFormBody(key, value) {
+  return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+}
+
+function formBody(data) {
+  return Object.keys(data).map((key) => {
+    return singleParamFormBody(key, data[key]);
+  }).join('&');
+}
+
+function formFetchParams(data) {
+  return {
+    body: formBody(data),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+  };
+}
+
+function apiFetch(endpoint, fetchParams) {
   const baseURL = 'https://api.feedreader.co/v1';
   const url = `${baseURL}/${endpoint}`;
   return fetch(url, fetchParams)
-    .then(res => res.json());
+    .then(res => res.json())
+    .then((json) => {
+      if (!json.success) throw e;
+      return json;
+    })
+    .catch(e => { throw e });
 }
+
+function apiFormFetch(endpoint, params, method) {
+  const fetchParams = formFetchParams(params);
+  return apiFetch(endpoint, {...fetchParams, method });
+}
+
+const api = {
+  get: (endpoint) => apiFetch(endpoint, { method: 'GET' }),
+  post: (endpoint, params) => apiFormFetch(endpoint, params, 'POST'),
+  del: (endpoint, params) => apiFormFetch(endpoint, params, 'DELETE'),
+};
 
 export default (username, token) => {
   return {
+    get: api.get,
     articles: {
-      get: (id) => api(`articles/${id}`, { method: 'GET' }),
+      get: (id) => api.get(`articles/${id}`),
     },
     feeds: {
       get: (url) => {
@@ -18,30 +54,34 @@ export default (username, token) => {
     },
     user: {
       tokens: {
-        create: (password) => {
-          const body = `password=${encodeURIComponent(password)}`;
-          const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-          };
-          return api(`${username}/tokens`, { method: 'POST', body, headers });
-        },
+        create: (password) => api.post(`${username}/tokens`, { password }),
         delete: () => {
         },
       },
       feeds: {
-        get: () => api(`${username}/feeds`, { method: 'GET' }),
+        get: () => api.get(`${username}/feeds`),
       },
       labels: {
         create: () => {
         },
-        get: (label) => api(`${username}/labels/${label}`, { method: 'GET' }),
+        get: (label) => api.get(`${username}/labels/${encodeURIComponent(label)}`),
+        post: (label, hash) => {
+          const endpoint = `${username}/labels/${encodeURIComponent(label)}`;
+          return api.del(endpoint, { hash });
+        },
       },
       folders: {
         create: () => {
         },
-        delete: () => {
+        get: (feed) => api.get(`${username}/folders?${feed}`),
+        post: (folder, xmlurl) => {
+          const endpoint = `${username}/folders/${encodeURIComponent(folder)}`;
+          return api.post(endpoint, { xmlurl });
         },
-        get: (feed) => api(`${username}/folders?${feed}`, { method: 'GET' }),
+        del: (folder, xmlurl) => {
+          const endpoint = `${username}/folders/${encodeURIComponent(folder)}`;
+          return api.del(endpoint, { xmlurl });
+        },
       },
     },
   };

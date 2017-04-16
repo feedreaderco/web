2ab7c3dbb2111c7c47fa529adfd94821c459359a
Arpith Siromoney <arpith@feedreader.co>
2017-04-16 22:12:36 +0530

    userpage: switch to es6

--- src/userpage.js
+++ src/userpage.js
@@ -1,64 +1,58 @@
 import api from './api';
 
-var h2 = document.getElementById('user');
-var read = document.getElementById('read');
-var user = window.location.pathname.split('/')[1];
+const h2 = document.getElementById('user');
+const read = document.getElementById('read');
+const user = window.location.pathname.split('/')[1];
 
-function get_labels() {
-  api('GET', user + '/labels', function(response) {
-    var div = document.getElementById('labels');
-    if (response.labels) {
-      if (!response.labels.length) {
-        div.innerHTML = "No labels.";
-      } else {
-        div.innerHTML = response.labels.map(function(label) {
-          return "<a href=/" + user + "/labels/" + encodeURIComponent(label) + " class=pillbox>" + label + "</a>";
-        }).join(' ');
-      }
+function getLabels() {
+  const div = document.getElementById('labels');
+  api(user).user.labels.get().then((response) => {
+    if (!response.labels.length) {
+      div.innerHTML = 'No labels';
     } else {
-      div.innerHTML = "Couldn't load labels.";
+      div.innerHTML = response.labels.map((label) => {
+        return `<a href=/${user}/labels/${encodeURIComponent(label)} class=pillbox>${label}</a>`;
+      }).join(' ');
     }
+  }).catch(() => {
+    div.innerHTML = 'Couldn\'t load labels';
   });
 }
 
-function get_folders() {
-  api('GET', user + '/folders', function(response) {
-    var div = document.getElementById('folders');
-    if (response.folders) {
-      if (!response.folders.length) {
-        div.innerHTML = "No folders.";
-      } else {
-        div.innerHTML = response.folders.map(function(folder) {
-          return "<a href=/" + user + "/folders/" + encodeURIComponent(folder) + " class=pillbox>" + folder + "</a>";
-        }).join(' ');
-      }
+function getFolders() {
+  const div = document.getElementById('folders');
+  api(user).user.folders.get().then((response) => {
+    if (!response.folders.length) {
+      div.innerHTML = 'No folders';
     } else {
-      div.innerHTML = "Couldn't load folders.";
+      div.innerHTML = response.folders.map((folder) => {
+        return `<a href=/${user}/folders/${encodeURIComponent(folder)} class=pillbox>${folder}</a>`;
+      }).join(' ');
     }
+  }).catch(() => {
+    div.innerHTML = 'Couldn\'t load folders';
   });
 }
 
-function get_feeds() {
-  api('GET', user + '/feeds', function(response) {
-    var ul = document.getElementById('feeds');
-    if (response.feeds) {
-      if (!response.feeds.length) {
-        ul.innerHTML = "No feeds.";
-      } else {
-        ul.innerHTML = response.feeds.map(function(feed) {
-          return "<li><a href=/feeds/" + encodeURIComponent(feed.key) + ">" + feed.title + "</a></li>";
-        }).join(' ');
-      }
+function getFeeds() {
+  const ul = document.getElementById('feeds');
+  api(user).user.feeds.get().then((response) => {
+    if (!response.feeds.length) {
+      ul.innerHTML = 'No feeds';
     } else {
-      ul.innerHTML = "Couldn't load feeds.";
+      ul.innerHTML = response.feeds.map((feed) => {
+        return `<li><a href=/feeds/${encodeURIComponent(feed.key)}>${feed.title}</a></li>`;
+      }).join(' ');
     }
+  }).catch(() => {
+    ul.innerHTML = 'Couldn\'t load feeds';
   });
 }
 
 h2.innerHTML = user;
-document.title = user + ' (feedreader.co)';
-read.href = "/" + user + "/labels/read";
+document.title = `${user} (feedreader.co)`;
+read.href = `/${user}/labels/read`;
 
-get_labels();
-get_folders();
-get_feeds();
+getLabels();
+getFolders();
+getFeeds();

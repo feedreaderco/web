df5aa1ba30c16f8aadf88a408aa98906ab8dbde8
Arpith Siromoney <arpith@feedreader.co>
2017-04-17 10:59:56 +0530

    article.js: camelCase

--- src/article.js
+++ src/article.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ArticleBody from './components/ArticleBody';
 
-const pathname_split = window.location.pathname.split('/');
-const hash = pathname_split[pathname_split.length - 2];
+const pathnameSplit = window.location.pathname.split('/');
+const hash = pathnameSplit[pathnameSplit.length - 2];
 const mountNode = document.getElementById('articles');
 
 ReactDOM.render(<ArticleBody id={hash} />, mountNode);

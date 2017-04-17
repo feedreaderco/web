dd2d3b435815684d192572e35563d0806ea27be3
Arpith Siromoney <arpith@feedreader.co>
2017-04-17 11:00:31 +0530

    server.js: arrow functions instead of anon functions

--- src/server.js
+++ src/server.js
@@ -11,38 +11,38 @@ const port = process.env.PORT || 8000;
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
-app.get('/articles/:id/body', function(req, res) {
+app.get('/articles/:id/body', (req, res) => {
   api().articles.get(req.params.id).then(({ article }) => {
     const reactString = renderToString(<ArticleBody article={article} />);
     res.render('article', { reactString });
   });
 });
 
-app.get('/articles/:articleID', function(req, res) {
+app.get('/articles/:articleID', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/label.html'));
 });
 
-app.get('/bookmarklet/js', function(req, res) {
+app.get('/bookmarklet/js', (req, res) => {
   res.redirect(301, '../bookmarklet.js');
 });
 
-app.get('/:user/labels/:label*', function(req,res) {
+app.get('/:user/labels/:label*', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/label.html'));
 });
 
-app.get('/:user/folders/:folder*', function(req,res) {
+app.get('/:user/folders/:folder*', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/label.html'));
 });
 
-app.get('/:user/feeds*', function(req,res) {
+app.get('/:user/feeds*', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/label.html'));
 });
 
-app.get('/feeds/*', function(req,res) {
+app.get('/feeds/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/label.html'));
 });
 
-app.get('/:user', function(req,res) {
+app.get('/:user', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/user.html'));
 });
 

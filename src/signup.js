a6e3c3afde1de8af5097f16230bf9ab216d682fa
Arpith Siromoney <arpith@feedreader.co>
2017-04-17 14:05:38 +0530

    use new api lib for signup

--- src/signup.js
+++ src/signup.js
@@ -1,22 +1,23 @@
 import api from './api';
 
 const signupButton = document.getElementById('signup');
-function signup() {
+
+function signup(e) {
+  e.preventDefault();
   const form = document.getElementById('signupForm');
-  const user = form.elements.user.value;
+  const username = form.elements.user.value;
   const password = form.elements.password.value;
-  localStorage.user = user;
-  api('POST', 'signup', (response) => {
-    if (response.token) {
-      form.style.display = 'none';
-      localStorage.token = response.token;
-      window.location.href = 'https://feedreader.co/welcome/';
-    } else {
-      console.log(response);
-      alert('Something went wrong - please contact me at arpith@feedreader.co');
-    }
-  }, `user=${user}&password=${password}`);
-  return false;
+  localStorage.user = username;
+  console.log(username, password);
+  api(username).signup(password).then((response) => {
+    if (!response.token) throw new Error();
+    form.style.display = 'none';
+    localStorage.token = response.token;
+    window.location.href = 'https://feedreader.co/welcome/';
+  }).catch((err) => {
+    console.error(err);
+    alert('Something went wrong - please contact me at arpith@feedreader.co');
+  });
 }
 
 signupButton.onclick = signup;

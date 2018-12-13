46b382c5fcf23be49efe7a4cfcfb030723a67278
Arpith Siromoney <arpith@feedreader.co>
2018-12-13 18:41:03 -0500

    Replace Label button with Favorite button

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ArticleTitle from './ArticleTitle';
 import FeedTitle from './FeedTitle';
 import ArticleBody from './ArticleBody';
-import Labels from './Labels';
+import FavoriteButton from './FavoriteButton';
 
 export default ({ article, labels, user }) => <div className="article">
   <ArticleTitle title={article.title} link={article.link} />
   <FeedTitle feedURL={article.feedurl} feedTitle={article.meta.title} />
   <ArticleBody body={article.description} />
-  <Labels articleID={article.hash} labels={labels} user={user} />
+  <FavoriteButton hash={article.hash} />
 </div>;
---
+++ src/styles/favoriteButton.js
@@ -0,0 +1,5 @@
+export default {
+  width: '1.7em',
+  height: '1.7em',
+  cursor: 'pointer',
+}

e9cf6e27935a5a835f9fc388a748804412b24061
Arpith Siromoney <arpith@feedreader.co>
2018-12-13 21:18:36 -0500

    Article: set isSelectedInitially for fav button

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,9 +4,13 @@ import FeedTitle from './FeedTitle';
 import ArticleBody from './ArticleBody';
 import FavoriteButton from './FavoriteButton';
 
+function isFavorite(labels, hash) {
+  return labels['favorites'] && labels['favorites'].indexOf(hash) > -1;
+}
+
 export default ({ article, labels, user }) => <div className="article">
   <ArticleTitle title={article.title} link={article.link} />
   <FeedTitle feedURL={article.feedurl} feedTitle={article.meta.title} />
   <ArticleBody body={article.description} />
-  <FavoriteButton hash={article.hash} />
+  <FavoriteButton hash={article.hash} isSelectedInitially={isFavorite(labels, article.hash)} />
 </div>;

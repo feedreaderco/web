034d49a79976f4746a16c6529683574d409b63e1
Arpith Siromoney <arpith@feedreader.co>
2018-12-17 09:10:46 -0500

    delete unused (duplicated) components folder

--- components/styles/PillBoxEmpty.js
+++
@@ -1,9 +0,0 @@
-import PillBox from './Pillbox';
-
-const PillBoxEmpty = Object.assign(PillBox, {
-  background: 'white',
-  color: '#4A525A',
-  borderColor: '#D1D0CE'
-}});
-
-export default PillBoxEmpty;
